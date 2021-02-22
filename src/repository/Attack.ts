3134fb7521eb4fc984b26344e7c8030fb6e20471
Vit Dolinek <vit.dolinek@qcm.cz>
2021-02-23 00:20:04 +0100

    attack log ordered by time

--- src/repository/Attack.ts
+++ src/repository/Attack.ts
@@ -4,6 +4,9 @@ import { Attack } from "../entity/Attack";
 @EntityRepository(Attack)
 export class AttackRepository extends Repository<Attack> {
   findByIdAndMarket = (id: number, market: string) => {
-    return this.find({ where: { playerId: id, market } });
+    return this.find({
+      where: { playerId: id, market },
+      order: { time: "DESC" },
+    });
   };
 }
